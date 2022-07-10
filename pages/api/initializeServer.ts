c16c920b617bbaa9aa6f4223ba028c7a89038770
Javan Ang <javan.ang@outlook.com>
2022-07-09 22:46:58 -0500

    pulling dev

--- pages/api/initializeServer.ts
+++ pages/api/initializeServer.ts
@@ -3,6 +3,6 @@ const exec = require('child_process').exec;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    exec('docker compose up');
+    exec('docker-compose up');
   };
 }
