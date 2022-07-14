f40662ab287c248bbdf606b2f483e0c44499524a
Javan Ang <javan.ang@outlook.com>
2022-07-13 19:40:48 -0500

    pulling from dev

--- pages/api/initializeServer.ts
+++ pages/api/initializeServer.ts
@@ -10,20 +10,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     let isRunning = false;
     let serverHealth;
-    
-    while(!isRunning){
+
+    while (!isRunning) {
       await sleep(5000);
       serverHealth = await serverInit.checkServerStatus();
-      if (serverHealth.status === 200) isRunning = true;
+      if (serverHealth !== undefined) {
+        if (serverHealth.status === 200) isRunning = true;
+      };
     };
-    
+
     const response = await serverInit.dropStream();
     if (response.commandStatus.status === 'SUCCESS') {
       const streamStatus: ClientResponse = await serverInit.createOrderStream();
       const unpaidTableStatus: ClientResponse = await serverInit.unpaidOrdersTable();
       const paidTableStatus: ClientResponse = await serverInit.paidOrdersTable();
 
-      if (streamStatus.status === 200 && unpaidTableStatus.status === 200 && paidTableStatus.status === 200) return res.status(200).json({status: 200});
+      if (streamStatus.status === 200 && unpaidTableStatus.status === 200 && paidTableStatus.status === 200) return res.status(200).json({ status: 200 });
     }
     return res.status(200).json('Unsuccessful');
   };
