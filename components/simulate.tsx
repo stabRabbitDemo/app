94c7ad4336ada9b87977e04d7c21482b3528d018
Gerry Bong <ggbong734@gmail.com>
2022-07-16 23:38:33 -0700

    changed button color

--- components/simulate.tsx
+++ components/simulate.tsx
@@ -44,13 +44,17 @@ const SimulateButton: FC<{ setRefreshData: Function }> = memo(({ setRefreshData 
       <Button
         id="createOrderButton"
         variant="outlined"
-        sx={{ backgroundColor: '#bbdefb', padding: "1rem", mx: "1rem", fontSize: "medium", fontWeight: 600, width: "16rem", mb: 1, border: "2px solid" }}
+        sx={{ color: "#2196f3", backgroundColor: '#e3f2fd', padding: "1rem", mx: "1rem", fontSize: "medium", fontWeight: 500, width: "16rem", mb: 2, border: "2px solid" }}
         onClick={() => setIntervalId(handleStart())}
       ><PlayArrowIcon style={{ 'color': "#1e88e5" }} sx={{ mr: "1rem" }} />Start Simulation</Button>
       <Button
         id="createOrderButton"
         variant="outlined"
-        sx={{ backgroundColor: '#ffcc80', padding: "1rem", mx: "1rem", fontSize: "medium", fontWeight: 600, width: "16rem", border: "2px solid" }}
+        sx={{
+          color: "#fb8c00", backgroundColor: '#fff3e0', padding: "1rem", mx: "1rem", fontSize: "medium", fontWeight: 500, width: "16rem", border: "2px solid #fb8c00", ':hover': {
+            bgcolor: '#ffebee'
+          },
+        }}
         onClick={() => {
           clearInterval(intervalId);
         }}
